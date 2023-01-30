8b6b48ddd0e8b552477757e245615c2eeb62684b
jeongboram <pink0913@nate.com>
2023-01-30 18:38:19 +0900

    feat: 회원가입 기능

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
+import { User } from './user.entity';
 import { UserRepository } from './user.repository';
 
 @Injectable()
 export class AuthService {
 	constructor(private userRepository: UserRepository) {}
+
+	async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
+		return this.userRepository.createUser(authCredentialDto);
+	}
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,16 @@
 import { CustomRepository } from 'src/boards/typeorm-ex.decorator';
 import { Repository } from 'typeorm';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
 
 @CustomRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+	async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
+		const { username, password } = authCredentialDto;
+		const user = this.create({
+			username,
+			password,
+		});
+		await this.save(user);
+	}
+}
