3cdb255cb5f6406a0fcc0927060ce331bc0ecdcb
jeongboram <pink0913@nate.com>
2023-01-31 16:29:40 +0900

    feat: auth 비밀번호 암호화

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -3,14 +3,18 @@ import { CustomRepository } from 'src/boards/typeorm-ex.decorator';
 import { Repository } from 'typeorm';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
 	async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
 		const { username, password } = authCredentialDto;
+
+		const salt = await bcrypt.genSalt();
+		const hashedPassword = await bcrypt.hash(password, salt);
 		const user = this.create({
 			username,
-			password,
+			password: hashedPassword,
 		});
 
 		try {
